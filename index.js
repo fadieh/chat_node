077d4ce7e165534181e4bb71740ef30d9dc75774
Fadie Hannona <fadiehannona@gmail.com>
2014-10-27 18:28:20 +0000

    Refactor route handler to use sendFile instead for index.html

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
+// Express initialises app to be a function handler so
+// can supply to a HTTP server.
 
 app.get('/', function(req, res){
-	res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname + '/index.html');
 });
+// Define a route handle / that gets called when we hit home.
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
+// We make the app listen on port 3000
